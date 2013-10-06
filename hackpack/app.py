91824a72340f2fd8b4ae4909b0cab23498e9b4c5
JRomer94 <jromer94@gmail.com>
2013-10-06 00:12:23 -0400

    changed body to Body

--- hackpack/app.py
+++ hackpack/app.py
@@ -31,7 +31,7 @@ def sms():
     bot = factory.create(ChatterBotType.CLEVERBOT)
     botSession = bot.create_session()
     
-    text = request.args.get('body', '') 
+    text = request.args.get('Body', '') 
     text = botSession.think(text)
 
     response = twiml.Response()
